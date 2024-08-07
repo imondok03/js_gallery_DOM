630de88db80fc76d7473f202c3dbb32b57c1da49
Ivan Mondok <ivanmondok8@gmail.com>
2024-08-07 18:12:51 +0300

    make build

---
+++ index.c51375e8.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("thumbs"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var n=e.target.closest("a");n&&(t.src=n.href)});
+//# sourceMappingURL=index.c51375e8.js.map
